ad0376e32d9dee3dffda34db750a23c9d3ce9cf3
Laiwer <pronik20075.0@gmail.com>
2021-12-05 11:10:45 +0300

    v0.2

--- dataBase/base.py
+++ dataBase/base.py
@@ -7,23 +7,23 @@ class BotDb():
         self.cursor = self.conn.cursor()
 
     def get_is_reg_user(self, user_id):
-        result = self.cursor.execute("SELECT id FROM users WHERE user_id == ?", (user_id,))
+        result = self.cursor.execute("SELECT id FROM users WHERE user_id == ?;", (user_id,))
         return bool(len(result.fetchall()))
 
     def add_user(self, user_id, full_name):
-        self.cursor.execute("INSERT INTO users VALUES (?, ?)", (user_id, full_name))
-        self.cursor.execute("INSERT INTO records VALUES (?, ? ?)", (user_id, 'русский', 'английский'))
+        self.cursor.execute("INSERT INTO users VALUES (?, ?);", (user_id, full_name))
+        self.cursor.execute("INSERT INTO records VALUES (?, ?, ?);", (user_id, 'русский', 'английский'))
         return self.conn.commit()
 
     def set_languages(self,user_id, from_lang, to_lang):
         if from_lang is None:
-            self.cursor.execute("UPDATE records SET to_lang = ? WHERE user_id = ?", (to_lang, user_id))
+            self.cursor.execute("UPDATE records SET to_lang = ? WHERE user_id = ?;", (to_lang, user_id))
         elif to_lang is None:
-            self.cursor.execute("UPDATE records SET from_lang = ? WHERE user_id = ?", (from_lang, user_id))
+            self.cursor.execute("UPDATE records SET from_lang = ? WHERE user_id = ?;", (from_lang, user_id))
         return self.conn.commit()
 
     def get_languages(self, user_id, language="f"):
-        from_lang = self.cursor.execute("SELECT from_lang FROM records WHERE user_id = ?", (user_id,))
-        to_lang = self.cursor.execute("SELECT to_lang FROM records WHERE user_id = ?", (user_id,))
+        from_lang = self.cursor.execute("SELECT from_lang FROM records WHERE user_id = ?;", (user_id,))
+        to_lang = self.cursor.execute("SELECT to_lang FROM records WHERE user_id = ?;", (user_id,))
         # условие в одну строчку
         return from_lang if language == "f" else to_lang
