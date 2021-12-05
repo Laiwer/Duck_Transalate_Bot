d418e4562fbc6edc2e3e37165d02e7a0e6eb62a1
Laiwer <pronik20075.0@gmail.com>
2021-12-05 10:04:25 +0300

    v0.2

--- loader.py
+++ loader.py
@@ -1,10 +1,10 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from googletrans import Translator
-from data import dataConfig
+from data import data_config
 from dataBase.base import BotDb
 
-bot = Bot(token=dataConfig.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
+bot = Bot(token=data_config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
