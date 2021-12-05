d9053aedca7d48835572b18a7bf847012c0b0e60
Laiwer <pronik20075.0@gmail.com>
2021-12-05 11:04:46 +0300

    v0.2

--- loader.py
+++ loader.py
@@ -3,6 +3,8 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from googletrans import Translator
 from data.config_data_top_secret import BOT_TOKEN
 from dataBase.base import BotDb
+import os.path
+
 
 bot = Bot(token=BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
@@ -10,4 +12,6 @@ dp = Dispatcher(bot, storage=storage)
 
 tsl = Translator()  # service_urls=['translate.googleapis.com']
 
-db = BotDb("dataBase/tanker.db")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(BASE_DIR, "dataBase/tanker.db")
+db = BotDb(db_path)
