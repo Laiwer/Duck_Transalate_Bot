e260b288fdc7c2c812634f9bfbc52ea16058b56e
Laiwer <pronik20075.0@gmail.com>
2021-12-05 10:50:04 +0300

    v0.2

--- utils/notify_admins.py
+++ utils/notify_admins.py
@@ -2,7 +2,7 @@ import logging
 
 from aiogram import Dispatcher
 
-from data.data_config123_top_secret import ADMINS
+from data.config_data_top_secret import ADMINS
 
 
 async def on_startup_notify(dp: Dispatcher):
