5ef8e295d2562d2d22cae94c97ed17417b3d4a20
Laiwer <pronik20075.0@gmail.com>
2021-12-14 22:47:20 +0300

    v0.2

--- loader.py
+++ loader.py
@@ -3,6 +3,7 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from googletrans import Translator
 from data import no_see_this_file_12345_data_config as nstf1dc
 from pymongo import MongoClient
+import dns
 
 
 bot = Bot(token=nstf1dc.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
