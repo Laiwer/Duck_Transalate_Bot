4c8baaad84843d9653f32c9a46c6421fb826e010
Laiwer <87445634+Laiwer@users.noreply.github.com>
2021-12-26 19:55:44 +0300

    Delete no_see_this_file_12345_data_config.py

--- data/no_see_this_file_12345_data_config.py
+++
@@ -1,3 +0,0 @@
-ADMINS = [1517124552]
-BOT_TOKEN = "2119740185:AAGtdxwMv1QoK5CM6r0BuNmKjCdKN5s5c_E"
-ip="mongodb+srv://Laiwer:xqSl1pvazZw3sH3x@clusterbot.c2rlb.mongodb.net/dataBase?retryWrites=true&w=majority"
