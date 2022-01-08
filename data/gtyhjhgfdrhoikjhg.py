6db149e14cb7e2164441c6ff4bdb36e6850d7996
Laiwer <87445634+Laiwer@users.noreply.github.com>
2022-01-08 10:41:40 +0300

    Delete gtyhjhgfdrhoikjhg.py

--- data/gtyhjhgfdrhoikjhg.py
+++
@@ -1,3 +0,0 @@
-ADMINS = [1517124552]
-BOT_TOKEN = "2119740185:AAEGw_7pN0AG9pICr1o2UOGbguDWXNr7Njo"
-ip="mongodb+srv://Laiwer:xqSl1pvazZw3sH3x@clusterbot.c2rlb.mongodb.net/dataBase?retryWrites=true&w=majority"
