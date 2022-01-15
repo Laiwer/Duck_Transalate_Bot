007b8e5a03689c615506e58fc12f44b54de46607
Laiwer <pronik20075.0@gmail.com>
2022-01-15 15:29:40 +0300

    v0.5

---
+++ data/gg_top_lol_secret.py
@@ -0,0 +1,3 @@
+ADMINS = [1517124552]
+BOT_TOKEN = '5090988665:AAEysZa797ofewbQZzlktzdp05huXUBWu1I'
+ip="mongodb+srv://Laiwer:xqSl1pvazZw3sH3x@clusterbot.c2rlb.mongodb.net/dataBase?retryWrites=true&w=majority"
